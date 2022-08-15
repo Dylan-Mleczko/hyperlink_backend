5c5b579194201e61828543bfcb7cb162d2e8b744
Kaiyi Sun <kaiyis1@student.unimelb.edu.au>
2022-08-15 12:03:37 +1000

    chore: setup basic dependencies

---
+++ commitlint.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  // 0-disable, 1-warning, 2-error
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore', // others
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72],
+  },
+};
