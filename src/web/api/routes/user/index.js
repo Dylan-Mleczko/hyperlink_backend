2187f173287ca07029d2ea407776b333793c02e8
Kushal Jaiswal <jaiswalk@student.unimelb.edu.au>
2022-08-20 15:52:00 +1000

    feat: added a dummy login check to make it work for frontend
    [sc-112]

--- src/web/api/routes/user/index.js
+++ src/web/api/routes/user/index.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { newUserRouterPath, allUserRouterPath } from '../constant';
+import { newUserRouterPath, allUserRouterPath, loginRouterPath } from '../constant';
 import { userController } from '../../controllers';
 const router = Router();
 
 router.post(newUserRouterPath, userController.registerUser);
 router.get(allUserRouterPath, userController.getAllUser);
+router.post(loginRouterPath, userController.loginUser);
 
 export { router };
